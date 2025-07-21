18392d31e5849b845e1567a332286403760cd508
amiralikhan34 <amiralidm1390@gmail.com>
2025-07-21 14:32:15 -0700

    Switch to PostgreSQL

--- resturant/settings.py
+++ resturant/settings.py
@@ -75,11 +75,11 @@ WSGI_APPLICATION = 'resturant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
